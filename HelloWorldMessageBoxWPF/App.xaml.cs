8eee891c7aa39119b3e610a6c9a0eea9e51cb187
Your Name <you@example.com>
2024-12-05 16:33:48 +0200

    first commit

---
+++ HelloWorldMessageBoxWPF/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace HelloWorldMessageBoxWPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ HelloWorldMessageBoxWPF/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HelloWorldMessageBoxWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        [DllImport("user32.dll")]
+        private static extern int MessageBox(IntPtr hWnd, string text, string caption, uint type);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox(IntPtr.Zero, "Hello, World!", "Message", 0);
+        }
+    }
+}
